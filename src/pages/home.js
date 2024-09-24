58f8779ca07b3df1df10c4d518f44d2cfcd2ab10
goldmaster631026 <goldmaster631026@gmail.com>
2024-09-25 02:34:04 +1000

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -41,7 +41,7 @@ const Home = () => {
     return () => {
       window.removeEventListener('scroll', handleMouseScroll);
     };
-  }, []); // Empty dependency array ensures this runs once on mount
+  }, []); // Empty dependency array ensures this runs once on mount mount
 
   useEffect(() => {
     const fetchUserData = async () => {
