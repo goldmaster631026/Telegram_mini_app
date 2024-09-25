37ca54f37b290b5d24813847133cf2cdef90d066
goldmaster631026 <goldmaster631026@gmail.com>
2024-09-25 21:40:53 +1000

    update

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -4,9 +4,11 @@ const Wallet = () => {
     <div>
       <div className="h-[90vh] w-[100vw]">
         <div>This is the Wallet Page.</div>
+        
       </div>
     </div>
   );
 };
 
+ // State variable to track if the mouse has scrolled to the top
 export default Wallet;
