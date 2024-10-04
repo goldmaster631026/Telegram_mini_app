bbc715106794c6f39e2e19ace49412113b0058c9
goldmaster631026 <goldmaster631026@gmail.com>
2024-10-05 06:10:04 +1000

    Update

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -12,5 +12,5 @@ const Wallet = () => {
 
  // State variable to track if the mouse has scrolled to the top
  // Add event listener for mouse scroll
-  // Add event listener for mouse scroll
+  // Add event listener for moS
 export default Wallet;
